ef88dc2e7bdccba27c3c75a003904413a3613fcc
drmayu <drnaufal88@gmail.com>
2023-01-04 10:08:23 +0800

    added imputed wisconsin data

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import preprocess
 
 breast_ca = pd.read_csv('Dataset/breast-cancer-wisconsin.data',index_col=None,header=None)
 df_impute = preprocess.clean_impute(breast_ca)
+
+#df_impute.set_index('sample_id',inplace=True)
+df_impute.to_csv('Dataset/processed/breast-cancer-wisconsin-imputed.csv',index=False)
