9bcb47720b65b204a980c587dca2d4b7cd6d7eba
drmayu <drnaufal88@gmail.com>
2023-01-06 00:58:09 +0800

    remove main.py and replaced with main.ipynb

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import preprocess
-
-## Read the wisconsin dataset
-breast_ca = pd.read_csv('Dataset/breast-cancer-wisconsin.data',index_col=None,header=None)
-
-## Run clean_impute function on dataset and save dataset
-df_impute = preprocess.clean_impute(breast_ca)
-df_impute.to_csv('Dataset/processed/breast-cancer-wisconsin-imputed.csv',index=False)
