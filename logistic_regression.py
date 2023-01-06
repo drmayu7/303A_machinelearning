9e08b8e4285701e04c471c53321c7a8348eda845
drmayu <drnaufal88@gmail.com>
2023-01-06 14:02:08 +0800

    add ROC plot and AOC calculation

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,8 +3,9 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import LeaveOneOut
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report,roc_curve,roc_auc_score
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Read imputed datasets (MICE)
 df = pd.read_csv("Dataset/processed/breast-cancer-wisconsin-imputed.csv")
@@ -13,8 +14,9 @@ df = pd.read_csv("Dataset/processed/breast-cancer-wisconsin-imputed.csv")
 df = df.drop(columns=['sample_id'])
 
 # Split the dataset into features/variables (X) and labels/outcome (y)
+# Change values of 'class' from [2,4] to [0,1]
 X = df.drop(columns=['class'])
-y = df['class']
+y = df['class'].apply(lambda x: 1 if x == 4 else 0)
 
 # Scale the features - Min and Max scaling (sensitive to outliers)
 scaler = MinMaxScaler()
@@ -26,8 +28,9 @@ loo = LeaveOneOut()
 # Run supervised learning - Logistic Regression algorithm
 model = LogisticRegression()
 
-# LOO method use multiple observations of evaluation, hence need to initialize a list to store 'class' prediction from all observations
+# LOO method use multiple observations of evaluation, hence need to initialize a list to store 'class' prediction and probability from all observations
 predictions = []
+probabilities = []
 
 # Loop through the cross-validation splits/observations
 for train_index, test_index in loo.split(X_scaled):
@@ -35,15 +38,34 @@ for train_index, test_index in loo.split(X_scaled):
     X_train, X_test = X_scaled[train_index], X_scaled[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-    # Fit the model on the training data and predict the labels for the test set
+    # Fit the model on the training data and predict the labels + probability for the test set
     model.fit(X_train,y_train)
     predicted_y = model.predict(X_test)
+    probability_y = model.predict_proba(X_test)[:,1]
 
-    # Append each predicted label and accuracy score into list
+    # Append each predicted label and probability score into list
     predictions.append(predicted_y[0])
+    probabilities.append(probability_y[0])
 
-# Convert the list to a Numpy Array
+# Convert the lists to a Numpy Array
 predictions = np.array(predictions)
+probabilities = np.array(probabilities)
+
+# Calculate false +ve rate, true +ve rate and threshold values
+fpr, tpr, thresholds = roc_curve(y,probabilities)
+
+# Calculate area under the curve
+roc_auc = roc_auc_score(y,probabilities)
+
+# Plot the ROC curve
+plt.plot(fpr, tpr, 'b', label='AUC = %0.2f' % roc_auc)
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
 
 # Calculate confusion matrix
 conf_mtx = confusion_matrix(y,predictions)
